009bfc4b07341b1c942d1ea351f8a8e336be6a6e
Zatosee <99033119+Zatosee@users.noreply.github.com>
2025-06-24 17:18:07 +0200

    avoid conflcit again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import LoginPage from "./pages/LoginPageZint";
-import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage.tsx";
 
 export default function App() {
   return (
