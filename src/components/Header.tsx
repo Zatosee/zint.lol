3300b262e488a17333e917134df47e56f69855ae
Zatosee <99033119+Zatosee@users.noreply.github.com>
2025-06-29 17:25:42 +0200

    Update maj

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,14 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-6 py-4 bg-opacity-40" style={{ backgroundColor: "#111a22" }}>
-      
-      <Link to="/">
-      <h1 className="text-xl font-bold text-cyan-400">Zint.lol</h1>
-      </Link>
 
+      <Link to="/">
+        <h1 className="text-xl font-light">
+          <span className="text-white">Zint</span>
+          <span className="text-[#00D0FF]">.lol</span>
+        </h1>
+      </Link>
+      
       <nav className="flex items-center space-x-6 text-sm">
         <a href="#" className="hover:text-cyan-400">Tier List</a>
         <a href="#" className="hover:text-cyan-400">Champions</a>
